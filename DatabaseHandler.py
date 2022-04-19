154d905e575799cf0a47ac9fabd14cc052aae21b
Richard Stuart <richard.stuart91@gmail.com>
2022-04-20 00:59:12 +0200

    table models, table add, insert

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,6 +1,48 @@
 import itertools
 from typing import List
+from typing import Any
+from typing import Dict
 import mysql.connector
+
+def PlantyModel() -> Dict[str,str]:
+    model = {"plant" : "plant VARCHAR(10) NOT NULL",
+    "motor_duration" : "motor_duration INT NOT NULL",
+    "motor_power" : "motor_power INT NOT NULL",
+    "temperature" : "temperature FLOAT NOT NULL",
+    "humidity" : "humidity FLOAT NOT NULL",
+    "light" : "light INT NOT NULL",
+    "moisture" : "moisture FLOAT NOT NULL",
+    "datetime" : "datetime DATETIME NOT NULL"
+    }
+    return model
+
+def CameraModel() -> Dict[str,str]:
+    model = {"original_pixel" : "original_pixel INT NOT NULL",
+    "green_pixel" : "green_pixel INT NOT NULL",
+    "green_percent" : "green_ercent FLOAT NOT NULL",
+    "datetime" : "datetime DATETIME NOT NULL"}
+    return model
+
+def SettingsModel() -> Dict[str,str]:
+    model = {"motor_duration" : "motor_duration INT NOT NULL",
+    "motor_power" : "motor_power INT NOT NULL",
+    "moisture_samples" : "moisture_samples INT NOT NULL",
+    "moisture_threshold" : "moisture_threshold INT NOT NULL",
+    "light_setpoint" : "light_setpoint INT NOT NULL",
+    "max_light" : "max_light INT NOT NULL",
+    "datetime" : "datetime DATETIME NOT NULL"}
+    return model
+
+def TestModel() -> Dict[str,str]:
+    model = {"plant" : "plant VARCHAR(10) NOT NULL",
+    "motor_duration" : "motor_duration INT NOT NULL",
+    "motor_power" : "motor_power INT NOT NULL"}
+    return model
+
+class Table:
+    def __init__(self, name : str, model : Dict[str,str]) -> None:
+        self. name = name
+        self.model = model
 
 class DataBaseHandler:
     def __init__(self, host, user="root", password="password"):
@@ -92,6 +134,28 @@ class DataBaseHandler:
             print(f"DEBUG: {e}")
             return False
 
+    def insert_into_table(self, table_name: str, table: Dict[str,Any]):
+        insert_statement_start = f"INSERT INTO {table_name} "
+        insert_statement_colums = "("
+        for column in table.keys():
+            insert_statement_colums += f"{column},"
+        insert_statement_colums = insert_statement_colums[0:len(insert_statement_colums)-1]
+        insert_statement_colums += ") "
+        insert_statement_values = f"VALUES ("
+        for column in table.keys():
+            insert_statement_values += f"%s,"
+        insert_statement_values = insert_statement_values[0:len(insert_statement_values)-1]
+        insert_statement_values += ")"
+        insert_statement = insert_statement_start + insert_statement_colums + insert_statement_values
+        print(insert_statement)
+        print(type(table.values()))
+        try:
+            self.cursor.execute(insert_statement, list(table.values()))
+            return True
+        except mysql.connector.errors.ProgrammingError as e:
+            print(f"DEBUG: {e}")
+            return False
+
 if __name__ == "__main__":
     print("Test database handler")
     host = "localhost"
@@ -110,5 +174,21 @@ if __name__ == "__main__":
         print(database)
     databaseHandler.close_database_connection()
     databaseHandler.connect_to_database(database)
-    databaseHandler.create_table(table, columns)
-    databaseHandler.delete_table(table)
+    test_table = Table("test", TestModel())
+    databaseHandler.create_table(test_table.name, test_table.model.values())
+    test = {"plant" : "Basil2", "motor_duration" : 40000, "motor_power" : 100}
+    databaseHandler.insert_into_table(test_table.name, test)
+
+    '''    planty_table = Table("planty", PlantyModel())
+    camera_table = Table("camera", CameraModel())
+    settings_table = Table("settings", SettingsModel())
+    print("DEBUG new:")
+    databaseHandler.create_table(planty_table.name, planty_table.model.values())
+    databaseHandler.create_table(camera_table.name, camera_table.model.values())
+    databaseHandler.create_table(settings_table.name, settings_table.model.values())
+    print("DEBUG: insert table")
+    planty_values = ["Basil", 30000, 100, 24.00, 46.00, 15000, 500.00, "2019-12-19T22:07:25"]
+    databaseHandler.insert_into_table(planty_table.name, planty_table.model.keys(), planty_values)'''
+    #databaseHandler.delete_table(planty_table.name)
+    #databaseHandler.delete_table(camera_table.name)
+    #databaseHandler.delete_table(settings_table.name)
