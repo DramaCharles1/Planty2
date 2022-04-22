69bddc6a649ae666c7a2740afca0cbf40c241e0c
Richard Stuart <richard.stuart91@gmail.com>
2022-04-23 00:07:13 +0200

    Read from planty and save result

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 import numpy as np
 from CameraHandler import CameraHandler
@@ -9,19 +8,24 @@ from DatabaseHandler import CameraModel, DataBaseHandler, PlantyModel
 from DatabaseHandler import Table
 from DatabaseHandler import PlantySettingsModel
 from DatabaseHandler import CameraSettingsModel
+from PlantyLib import PlantyCommands
+from PlantyLib import Temp_option
 
 PLANTY_DATABASE = "Planty2"
 PLANTY_TABLE = "Planty_data"
 CAMERA_TABLE = "Camera_data"
 PLANTY_SETTINGS_TABLE = "Planty_settings"
 CAMERA_SETTINGS_TABLE = "Camera_settings"
+MOIS_SAMPLES = 5
 
 def main(settings_path, settings_file, camera, nightmode):
     timestamp = datetime.now().replace(microsecond=0).isoformat()
+    print(f"[DEBUG] {timestamp}")
     planty_settings = PlantySetings(settings_path, settings_file)
     camera_settings = CameraSettings(settings_path, settings_file)
     database_handler = DataBaseHandler()
     database_handler.connect()
+    planty_lib = PlantyCommands()
 
     if not database_handler.database_exist(PLANTY_DATABASE):
         database_handler.create_database(PLANTY_DATABASE)
@@ -89,7 +93,7 @@ def main(settings_path, settings_file, camera, nightmode):
     if camera:
         planty_camera = CameraHandler()
         image_name = timestamp
-        planty_camera.take_picture(camera_settings.picture_directory, image_name, nightmode=True)
+        planty_camera.take_picture(camera_settings.picture_directory, image_name, nightmode=False)
 
         original_color = PlantyColor(camera_settings.picture_directory, image_name)
         green_image = original_color.color_filter_image(camera_settings.lower_green_filter, camera_settings.upper_green_filter)
@@ -109,10 +113,31 @@ def main(settings_path, settings_file, camera, nightmode):
                         "datetime" : timestamp}
         database_handler.insert_into_table(CAMERA_TABLE, camera_result)
 
+    planty_result = {}
+    planty_result["plant"] = planty_lib.read_plant()
+    planty_result["temperature"] = planty_lib.read_temperature(Temp_option.TEMPERATURE)
+    planty_result["humidity"] = planty_lib.read_temperature(Temp_option.HUMIDITY)
+    planty_result["light"] = planty_lib.read_ALS()
+    planty_result["moisture"] = planty_lib.read_moisture(samples=MOIS_SAMPLES)
+
+    if planty_result["moisture"] <= new_planty_settings["moisture_threshold"] and not nightmode:
+        planty_lib.start_pump(True, planty_settings["motor_duration"], planty_settings["motor_power"])
+        planty_result["motor_duration"] = planty_settings["motor_duration"]
+        planty_result["motor_power"] = planty_settings["motor_power"]
+    elif planty_result["moisture"] > new_planty_settings["moisture_threshold"] and not nightmode:
+        planty_result["motor_duration"] = 0
+        planty_result["motor_power"] = 0
+    else:
+        planty_result["motor_duration"] = -1
+        planty_result["motor_power"] = -1
+
+    planty_result["datetime"] = timestamp
+    database_handler.insert_into_table(PLANTY_TABLE, planty_result)
+
 if __name__ == "__main__":
     #args: settings_path, settings_file, camera, nightmode
     print("main test")
-    test_argv = ["misc/", "settings.xml", True, False]
+    test_argv = ["misc/", "settings.xml", False, True]
 
     if len(test_argv) != 4:
         raise Exception(f"Arguements not correct: {test_argv}")
