133928d2edf79fd04612c57e3bd17341259b1e93
Richard Stuart <richard.niklas.stuart@gmail.com>
2022-06-18 15:33:54 +0200

    CHG: moisture sensors, plant read

--- applications/nano/Main_nano.py
+++ applications/nano/Main_nano.py
@@ -2,11 +2,21 @@ import sys
 from datetime import datetime
 from PlantyLib import PlantyCommands
 
+MOIS_SAMPLES = 5
+MOIS_SENSORS = 2
 
 def main():
     timestamp = datetime.now().replace(microsecond=0).isoformat()
     print(f"[START] {timestamp}")
     planty_lib = PlantyCommands("/dev/ttyUSB0")
+
+    planty_result = {}
+    planty_result["plant"] = planty_lib.read_plant()
+    print(planty_result["plant"])
+    for sensor in range(1, MOIS_SENSORS + 1):
+        moisture_result = planty_lib.read_moisture(sensor_number=sensor, samples=MOIS_SAMPLES)
+        print(moisture_result["sensor_number_return"])
+        print(moisture_result["sensor_read"])
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
