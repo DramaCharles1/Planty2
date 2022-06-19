84fda4794d3335296a94da83a317ee7e17ab7328
Richard Stuart <richard.stuart91@gmail.com>
2022-06-19 12:46:39 +0200

    CHG: data into database

--- applications/nano/Main_nano.py
+++ applications/nano/Main_nano.py
@@ -1,4 +1,3 @@
-import sys
 from datetime import datetime
 from PlantyLib import PlantyCommands
 from PlantyLib import Temp_option
@@ -14,8 +13,10 @@ MOIS_SENSORS = 2
 def main():
     timestamp = datetime.now().replace(microsecond=0).isoformat()
     print(f"[START] {timestamp}")
+
     database_handler = DataBaseHandler()
     database_handler.connect()
+
     if not database_handler.database_exist(DATABASE):
         database_handler.create_database(DATABASE)
     database_handler.close_database_connection()
@@ -24,28 +25,27 @@ def main():
     if not database_handler.table_exist(TABLE):
         new_table = Table(TABLE, NanoModel())
         database_handler.create_table(new_table.name, new_table.columns)
+        database_handler.insert_into_table(TABLE,{"entry" : 1})
 
     planty_lib = PlantyCommands("/dev/ttyUSB0")
     planty_result = {}
+    planty_result["datetime"] = timestamp
     plant = planty_lib.read_plant()
-    print(f"[DEBUG] Plant: {plant}")
     planty_result["plant_1"] = plant
     planty_result["plant_2"] = plant
-    
+
     for sensor in range(1, MOIS_SENSORS + 1):
         moisture_result = planty_lib.read_moisture(sensor_number=sensor, samples=MOIS_SAMPLES)
-        print(moisture_result["sensor_number_return"])
-        print(moisture_result["sensor_read"])
+        planty_result[f"moisture_{moisture_result['sensor_number_return']}"] = moisture_result["sensor_read"]
     temperature = planty_lib.read_temperature(Temp_option.TEMPERATURE)
-    print(f"[DEBUG] Temperature: {temperature}")
     planty_result["temperature"] = temperature
     humidity = planty_lib.read_temperature(Temp_option.HUMIDITY)
-    print(f"[DEBUG] Humidity: {humidity}")
     planty_result["humidity"] = humidity
+    new_entry = database_handler.select_from_table(TABLE, ["entry"], True, "Datetime", 1)[0][0] + 1
+    planty_result["entry"] = new_entry
+    print(planty_result)
+
+    database_handler.insert_into_table(TABLE, planty_result)
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        test_argv = ["misc/", "settings.xml", "True", "False", True]
-        print("main test")
-        main()
-        print("End main test")
+    main()
