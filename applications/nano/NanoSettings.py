573e89f48f0a931e116e6cd00829eac48287982a
Richard Stuart <richard.stuart91@gmail.com>
2022-06-29 23:42:51 +0200

    CHG: added image copy settings

--- applications/nano/NanoSettings.py
+++ applications/nano/NanoSettings.py
@@ -7,8 +7,12 @@ class NanoSettings(SettingsHandler):
         nano_settings = self.mydoc.getElementsByTagName('nano_settings')
         self._moisture_threshold = nano_settings[0].getElementsByTagName("moisture_threshold")[0].firstChild.data.strip()
         self._samples = nano_settings[0].getElementsByTagName("moisture_samples")[0].firstChild.data.strip()
+        self._picture_directory = nano_settings[0].getElementsByTagName("picture_directory")[0].firstChild.data.strip()
+        self._picture_copy_directory = nano_settings[0].getElementsByTagName("picture_copy_directory")[0].firstChild.data.strip()
         self.settings = {"moisture_samples" : self.moisture_samples,
                        "moisture_threshold" : self.moisture_threshold,
+                       "picture_directory" : self.picture_directory,
+                       "picture_copy_directory" : self.picture_copy_directory,
                         }
 
     @property
@@ -17,3 +21,9 @@ class NanoSettings(SettingsHandler):
     @property
     def moisture_threshold(self) -> float:
         return float(self._moisture_threshold)
+    @property
+    def picture_directory(self) -> str:
+        return self._picture_directory
+    @property
+    def picture_copy_directory(self) -> str:
+        return self._picture_copy_directory
