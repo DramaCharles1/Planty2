404e2d5432490fec4ad15059c503105bea77a836
Richard Stuart <richard.stuart91@gmail.com>
2022-06-29 22:28:23 +0200

    CHG: test program with arguements

--- applications/nano/Main_nano.py
+++ applications/nano/Main_nano.py
@@ -1,15 +1,15 @@
 from datetime import datetime
+from Model_nano import NanoModel
 from PlantyLib import PlantyCommands
 from DatabaseHandler import DataBaseHandler
 from DatabaseHandler import Table
-from Model_nano import NanoModel
 
 DATABASE = "nano"
 TABLE = "nano_data"
 MOIS_SAMPLES = 5
 MOIS_SENSORS = 2
 
-def main():
+def main(settings_path, settings_file, test=False):
     timestamp = datetime.now().replace(microsecond=0).isoformat()
     print(f"[START] {timestamp}")
 
@@ -42,4 +42,15 @@ def main():
     database_handler.insert_into_table(TABLE, planty_result)
 
 if __name__ == "__main__":
-    main()
+    import sys
+    if len(sys.argv) == 1:
+        test_argv = ["applications/nano/", "settings.xml", True]
+        print("main test")
+        main(test_argv[0], test_argv[1], test_argv[2])
+        print("End main test")
+    elif len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        if len(sys.argv) < 4:
+            raise Exception(f"[DEBUG] Not enough arguements: {sys.argv}")
+        raise Exception(f"[DEBUG] Too many arguments: {sys.argv}")
