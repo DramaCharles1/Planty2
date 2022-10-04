73bb57b076f4471cd3efceec5d96aa2b06a52422
Richard Stuart <richard.stuart91@gmail.com>
2022-10-04 22:45:48 +0200

    CHG: pylint and debug

--- applications/lights/Main_lights.py
+++ applications/lights/Main_lights.py
@@ -1,9 +1,9 @@
-import time
 from datetime import datetime
-from LightSettings import LightSetings
+from PlantyLib import Light_color_option
 from PlantyLib import PlantyCommands
+from LightSettings import LightSetings
 
-def main(settings_path, settings_file, nightmode,test=False):
+def main(settings_path, settings_file, nightmode):
     timestamp = datetime.now().replace(microsecond=0).isoformat()
     print(f"[START] {timestamp}")
     light_settings = LightSetings(settings_path, settings_file)
@@ -12,14 +12,16 @@ def main(settings_path, settings_file, nightmode,test=False):
     if not nightmode:
         planty_lib.light_regulator_values(True, light_settings.kp, light_settings.ki, light_settings.integration_time, light_settings.max_light)
         planty_lib.start_light_regulator(True, light_settings.light_setpoint)
+    else:
+        planty_lib.lights(True, Light_color_option.PURPLE, power=0)
 
 if __name__ == "__main__":
     import sys
     print("main test")
     if len(sys.argv) == 1:
-        test_argv = ["applications/planty/", "settings.xml", "True", True]
-        main(test_argv)
+        test_argv = ["applications/lights/", "settings.xml", True]
+        main(test_argv[0],test_argv[1],test_argv[2])
     else:
         print(f"[ARGUEMENTS] {sys.argv}")
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
     print("End main test")
